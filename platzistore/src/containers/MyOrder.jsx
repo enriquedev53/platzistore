9af916b6b43f7d72eea13e1bf3b305493ffaa630
Enrique Henandez <enriquedev53@gmail.com>
2022-03-01 11:41:11 -0600

    se realizacon modificaciones para migrar el proyecto

---
+++ platzistore/src/containers/MyOrder.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import OrderItem from '@components/OrderItem';
+import AppContext from '@context/AppContext';
+import arrow from '@icons/flechita.svg';
+import styles from '@styles/MyOrder.module.scss';
+
+const MyOrder = () => {
+	return (
+		<aside className={ styles.MyOrder }>
+			<div className="title-container">
+				<img src={arrow} alt="arrow" />
+				<p className="title">My order</p>
+			</div>
+			<div className="my-order-content">
+				<OrderItem />
+				<div className="order">
+					<p>
+						<span>Total</span>
+					</p>
+					<p>$560.00</p>
+				</div>
+				<button className="primary-button">
+					Checkout
+				</button>
+			</div>
+		</aside>
+	);
+}
+
+export default MyOrder;
