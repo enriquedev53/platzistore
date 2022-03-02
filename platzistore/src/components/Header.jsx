e213be23791cc26ef55eac42b7df62188d8c37af
Enrique Henandez <enriquedev53@gmail.com>
2022-03-01 20:02:33 -0600

    Se hicieron modificaciones en los archivos de estilos y componentes

--- platzistore/src/components/Header.jsx
+++ platzistore/src/components/Header.jsx
@@ -1,7 +1,8 @@
-import React, { useState } from 'react';
+import React, { useContext, useState } from 'react';
+import Link from 'next/link';
 import Image from 'next/image';
-import MyOrder from '@containers/MyOrder';
 import Menu from '@components/Menu';
+import MyOrder from '@containers/MyOrder';
 import menu from '@icons/icon_menu.svg';
 import logo from '@logos/logo_yard_sale.svg';
 import AppContext from '@context/AppContext';
@@ -13,48 +14,114 @@ const Header = () => {
 
 	const handleToggle = () => {
 		setToggle(!toggle);
-	}
+}
 
 	return (
-		<nav className={styles.Nav}>
-			<Image src={menu} alt="menu" className={styles.menu} />
-			<div className={styles['navbar-left']}>
-				<Image src={logo} alt="logo" className={styles['nav-logo']} />
-				<ul>
-					<li>
-						<a href="/">All</a>
-					</li>
-					<li>
-						<a href="/">Clothes</a>
-					</li>
-					<li>
-						<a href="/">Electronics</a>
-					</li>
-					<li>
-						<a href="/">Furnitures</a>
-					</li>
-					<li>
-						<a href="/">Toys</a>
-					</li>
-					<li>
-						<a href="/">Others</a>
-					</li>
-				</ul>
-			</div>
-			<div className={styles['navbar-right']}>
-				<ul>
-					<li className={styles['navbar-email']} onClick={handleToggle}>
-						platzi@example.com
-					</li>
-					<li className={styles['navbar-shopping-cart']}>
-						<Image src={shoppingCart} alt="shopping cart" />
-						<div>2</div>
-					</li>
-				</ul>
-			</div>
-			{toggle && <Menu />}
-		</nav>
+		<>
+			<nav className={styles.Nav}>
+				<Image src={menu} alt="menu" className={styles.menu} />
+				<div className={styles['navbar-left']}>
+					<Link href="/">
+						<Image src={logo} alt="logo" className={styles['nav-logo']} />
+					</Link>
+					<ul>
+						<li>
+							<a href="/">All</a>
+						</li>
+						<li>
+							<a href="/">Clothes</a>
+						</li>
+						<li>
+							<a href="/">Electronics</a>
+						</li>
+						<li>
+							<a href="/">Furnitures</a>
+						</li>
+						<li>
+							<a href="/">Toys</a>
+						</li>
+						<li>
+							<a href="/">Others</a>
+						</li>
+					</ul>
+				</div>
+				<div className={styles['navbar-right']}>
+					<ul>
+						<li className={styles['navbar-email']} onClick={handleToggle}>
+							platzi@example.com
+						</li>
+						<li className={styles['navbar-shopping-cart']}>
+							<Image src={shoppingCart} alt="shopping cart" />
+							<div>2</div>
+						</li>
+					</ul>
+				</div>
+				{toggle && <Menu />}
+			</nav>
+			{toggle && <MyOrder />}
+		</>
 	);
 }
 
 export default Header;
+
+// import React, { useState } from 'react';
+// import Image from 'next/image';
+// import MyOrder from '@containers/MyOrder';
+// import Menu from '@components/Menu';
+// import menu from '@icons/icon_menu.svg';
+// import logo from '@logos/logo_yard_sale.svg';
+// import AppContext from '@context/AppContext';
+// import shoppingCart from '@icons/icon_shopping_cart.svg';
+// import styles from '@styles/Header.module.scss';
+
+// const Header = () => {
+// 	const [toggle, setToggle] = useState(false);
+
+// 	const handleToggle = () => {
+// 		setToggle(!toggle);
+// 	}
+
+// 	return (
+// 		<nav className={styles.Nav}>
+// 			<Image src={menu} alt="menu" className={styles.menu} />
+// 			<div className={styles['navbar-left']}>
+// 				<Image src={logo} alt="logo" className={styles['nav-logo']} />
+// 				<ul>
+// 					<li>
+// 						<a href="/">All</a>
+// 					</li>
+// 					<li>
+// 						<a href="/">Clothes</a>
+// 					</li>
+// 					<li>
+// 						<a href="/">Electronics</a>
+// 					</li>
+// 					<li>
+// 						<a href="/">Furnitures</a>
+// 					</li>
+// 					<li>
+// 						<a href="/">Toys</a>
+// 					</li>
+// 					<li>
+// 						<a href="/">Others</a>
+// 					</li>
+// 				</ul>
+// 			</div>
+// 			<div className={styles['navbar-right']}>
+// 				<ul>
+// 					<li className={styles['navbar-email']} onClick={handleToggle}>
+// 						platzi@example.com
+// 					</li>
+// 					<li className={styles['navbar-shopping-cart']}>
+// 						<Image src={shoppingCart} alt="shopping cart" />
+// 						<div>2</div>
+// 					</li>
+// 				</ul>
+// 			</div>
+// 			{toggle && <Menu />}
+// 		</nav>
+// 	);
+// }
+
+// export default Header;
--- platzistore/src/components/OrderItem.jsx
+++ platzistore/src/components/OrderItem.jsx
@@ -1,17 +1,21 @@
 import React from 'react';
+import Image from 'next/image';
 import AppContext from '@context/AppContext';
 import close from '@icons/icon_close.png'
 import styles from '@styles/OrderItem.module.scss';
 
-const OrderItem = () => {
+const OrderItem = ({ product }) => {
 	return (
 		<div className={ styles.OrderItem }>
 			<figure>
-				<img src="https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="bike" />
+				<Image src="https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" 
+				width={940}
+				height={650}
+				alt="bike" />
 			</figure>
-			<p>Bike</p>
-			<p>$30,00</p>
-			<img src={close} alt="close" />
+			<p>{product?.title}</p>
+			<p>${product?.price}</p>
+			<Image src={close} alt="close" />
 		</div>
 	);
 }
