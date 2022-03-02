5b0aab5767351a7c28df1e79a427b9463e60d217
Enrique Henandez <enriquedev53@gmail.com>
2022-03-02 13:36:00 -0600

    Agrego codigo de Google Analtycs

--- platzistore/src/pages/_document.js
+++ platzistore/src/pages/_document.js
@@ -6,6 +6,15 @@ class MyDocument extends Document {
             <Html lang ='en'>
                 <Head>
                     <title>React Shop</title>
+                    <script async src="https://www.googletagmanager.com/gtag/js?id=G-RNLHXNGR67"></script>
+                    <script dangerouslySetInnerHTML={{
+                        html: `
+                        window.dataLayer = window.dataLayer || [];
+                          function gtag(){dataLayer.push(arguments);}
+                          gtag('js', new Date());
+                          gtag('config', 'G-RNLHXNGR67');
+                        `
+                    }} />
                 </Head>
                 <body>
                     <Main />
